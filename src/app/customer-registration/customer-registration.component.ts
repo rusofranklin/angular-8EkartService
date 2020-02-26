7ef81dca4156dceadae4c75c7810a103076b3b8a
rusofranklin <rusofranklin@gmail.com>
2020-02-26 10:10:09 +0530

    2 files modified

--- src/app/customer-registration/customer-registration.component.ts
+++ src/app/customer-registration/customer-registration.component.ts
@@ -1,20 +1,35 @@
-import { Component } from '@angular/core';
-import { NgModule } from '@angular/core';
-import { FormGroup,FormBuilder } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
+import { Component } from "@angular/core";
+import { NgModule } from "@angular/core";
+import { FormGroup, FormBuilder } from "@angular/forms";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'app-customer-registration',
-  templateUrl: './customer-registration.component.html',
-  styleUrls: ['./customer-registration.component.css']
+  selector: "app-customer-registration",
+  templateUrl: "./customer-registration.component.html",
+  styleUrls: ["./customer-registration.component.css"]
 })
 @NgModule({
-  imports:   [ HttpClient ],
+  imports: [HttpClient]
 })
 export class CustomerRegistrationComponent {
-first_name='';
- onClickSubmit(data) {this.first_name = data.first_name;
-  console.log(this.first_name);
+  firstname = "";
+
+  person = {
+    firstName: "",
+    lastName: "",
+    emailAddress: "",
+    areaCode: "",
+    phoneNumber: "",
+    presentAddress: ""
+  };
+  onClickSubmit(data) {
+    this.person.firstName = data.first_name;
+    this.person.lastName = data.last_name;
+    this.person.emailAddress = data.email_address;
+    this.person.areaCode = data.area_code;
+    this.person.phoneNumber = data.phone_number;
+    this.person.presentAddress = data.present_address;
+
+    console.log(data);
   }
 }
-
