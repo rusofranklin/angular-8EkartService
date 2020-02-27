d7afc1abf93b8fabbfe07172f85efdd1d876c29f
rusofranklin <rusofranklin@gmail.com>
2020-02-27 21:50:46 +0530

    1 file modified, 3 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,11 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { CustomerRegistrationComponent } from './customer-registration/customer-registration.component';
-import { VendorRegistrationComponent } from './customer-registration/vendor-registration/vendor-registration.component';
+
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule,ReactiveFormsModule,HttpClientModule ],
-  declarations: [ AppComponent, HelloComponent, CustomerRegistrationComponent, VendorRegistrationComponent ],
+  declarations: [ AppComponent, HelloComponent, CustomerRegistrationComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
