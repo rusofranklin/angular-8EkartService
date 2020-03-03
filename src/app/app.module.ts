4d3c50e3e53510f09728050fe1e2f05bb0c47b9e
rusofranklin <rusofranklin@gmail.com>
2020-03-03 22:06:02 +0530

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,20 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
-import { Route, RouterModule } from "@angular/router";
+import { RouterModule, Route } from "@angular/router";
 
 import { AppComponent } from "./app.component";
 import { HelloComponent } from "./hello.component";
 import { CustomerRegistrationComponent } from "./customer-registration/customer-registration.component";
 import { VendorRegistrationComponent } from "./vendor-registration/vendor-registration.component";
 
-const router: Route = [
-  { path: "Customer", component: CustomerRegistrationComponent },
-  { path: "Vendor", component: VendorRegistrationComponent }
+const routes: Routes = [
+  { path: 'customer', component: CustomerRegistrationComponent },
+  { path: 'vendor', component: VendorRegistrationComponent },
 ];
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, ReactiveFormsModule, HttpClientModule,RouterModule.forRoot(router)],
+  imports: [BrowserModule, FormsModule, RouterModule.forRoot(routes)],
   declarations: [
     AppComponent,
     HelloComponent,
