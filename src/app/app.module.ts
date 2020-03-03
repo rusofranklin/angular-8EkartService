038ee0d93801203047c732987926060642e452a7
rusofranklin <rusofranklin@gmail.com>
2020-03-03 21:34:39 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,27 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule,ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { Route, RouterModule } from "@angular/router";
 
+import { AppComponent } from "./app.component";
+import { HelloComponent } from "./hello.component";
+import { CustomerRegistrationComponent } from "./customer-registration/customer-registration.component";
+import { VendorRegistrationComponent } from "./vendor-registration/vendor-registration.component";
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
-import { CustomerRegistrationComponent } from './customer-registration/customer-registration.component';
-import { VendorRegistrationComponent } from './vendor-registration/vendor-registration.component';
-
-
+const router: Route = [
+  { path: "Customer", component: CustomerRegistrationComponent },
+  { path: "Vendor", component: VendorRegistrationComponent }
+];
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule,ReactiveFormsModule,HttpClientModule ],
-  declarations: [ AppComponent, HelloComponent, CustomerRegistrationComponent, VendorRegistrationComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule, HttpClientModule,RouterModule.forRoot(router)],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    CustomerRegistrationComponent,
+    VendorRegistrationComponent
+  ],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
